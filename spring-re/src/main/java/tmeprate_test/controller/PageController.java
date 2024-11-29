768c6a7da228a46255b8694d80476433e54b763d
penguindisc <n84066@gmail.com>
2024-11-29 18:12:41 +0900

    응애

--- spring-re/src/main/java/tmeprate_test/controller/PageController.java
+++ spring-re/src/main/java/tmeprate_test/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping({"/","/login"})
 public class PageController {
 	
 	@Value("${kakao.client_id}")
@@ -16,7 +16,7 @@ public class PageController {
 	@Value("${kakao.redirect_uri}")
 	private String redirect_uri;
 
-	@GetMapping("/page")
+	@GetMapping({"/","/page"})
 	public String loginPage(Model model) {
 		String location = "https://kauth.kakao.com/oauth/authorize?response_type=code"
 				+ "&client_id="+client_id 
